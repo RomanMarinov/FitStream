9f3208a964830667cdde295f544cf19b7b04eddd
romanmarinov <marinov37@mail.ru>
2025-07-15 00:06:22 +0300

    add ResourceProvider

---
+++ app/src/main/java/com/example/fitstream/utils/resource/ResourceProvider.kt
@@ -0,0 +1,7 @@
+package com.example.fitstream.utils.resource
+
+import androidx.annotation.StringRes
+
+interface ResourceProvider {
+    fun getResourceString(@StringRes resId: Int, vararg arg: Any) : String
+}
